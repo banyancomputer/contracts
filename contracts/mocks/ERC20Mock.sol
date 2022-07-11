f43113039155010f489e22c86ff19d0441737bc0
Marco Jardim <marcoeojardim@gmail.com>
2022-07-11 20:21:22 -0300

    adding erc20 mock (for testing)

---
+++ contracts/mocks/ERC20Mock.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+
+contract ERC20Mock is ERC20PresetFixedSupply {
+    constructor () ERC20PresetFixedSupply("MockERC20", "MOCK", 1000000, msg.sender) {
+    }
+}
--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,6 +1,7 @@
 export const CONTRACTS: Record<string, string> = {
     escrow: "Escrow",
     authority: "Authority",
+    erc20Mock: "ERC20Mock",
 };
 
 export const CONFIGURATION: Record<string, any> = {
