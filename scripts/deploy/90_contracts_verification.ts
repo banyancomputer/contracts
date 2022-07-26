103079eb9aa60ad487f019c9b8bcf621476ddbc7
Marco Jardim <marcoeojardim@gmail.com>
2022-07-26 12:00:34 -0300

    updating etherscan contract verification script

--- scripts/deploy/90_contracts_verification.ts
+++ scripts/deploy/90_contracts_verification.ts
@@ -15,6 +15,7 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     console.log("Account balance:", ethers.utils.formatEther((await signer.getBalance()).toString()) + " ETH");
 
     const authorityDeployment = await deployments.get(CONTRACTS.authority);
+    const treasuryDeployment = await deployments.get(CONTRACTS.treasury);
     const escrowDeployment = await deployments.get(CONTRACTS.escrow);
     
     const network = await ethers.provider.getNetwork();
@@ -50,8 +51,26 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
             console.log("Sleepin' for 30 seconds to wait for the chain to be ready...");
             await delay(30e3); // 30 seconds delay to allow the network to be synced
             await hre.run("verify:verify", {
+                address: treasuryDeployment.address,
+                constructorArguments: [authorityDeployment.address],
+            });
+            console.log("Verified -- treasury Contract");
+        } catch (error) {
+            if (error instanceof NomicLabsHardhatPluginError) {
+                // specific error
+                console.log("Error verifying -- treasury Contract");
+                console.log(error.message);
+            } else {
+                throw error; // let others bubble up
+            }                      
+        }
+
+        try {
+            console.log("Sleepin' for 30 seconds to wait for the chain to be ready...");
+            await delay(30e3); // 30 seconds delay to allow the network to be synced
+            await hre.run("verify:verify", {
                 address: escrowDeployment.address,
-                constructorArguments: [],
+                constructorArguments: [authorityDeployment.address],
             });
             console.log("Verified -- escrow Contract");
         } catch (error) {
