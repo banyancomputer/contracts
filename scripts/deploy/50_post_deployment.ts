ccccf3b61f0b2eeef3c100e3920720255e505e47
Marco Jardim <marcoeojardim@gmail.com>
2022-07-26 13:16:23 -0300

    adding post-deployment setup to ERC Mocks

--- scripts/deploy/50_post_deployment.ts
+++ scripts/deploy/50_post_deployment.ts
@@ -7,7 +7,11 @@ import {
     Authority__factory,
     Treasury__factory,
     Escrow__factory,
+    ERC20Mock__factory,
+    ERC721Mock__factory,
+    ERC1155Mock__factory
 } from "../../types";
+
 
 const delay = (ms: number | undefined) => new Promise(resolve => setTimeout(resolve, ms))
 
@@ -17,12 +21,18 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     const { deployer } = await getNamedAccounts();
     const signer = await ethers.provider.getSigner(deployer);
 
+    console.log("***** POST DEPLOYMENT TASKS *****");
+
     console.log("Account balance:", ethers.utils.formatEther((await signer.getBalance()).toString()) + " ETH");
+
+    const erc20MockDeployment = await deployments.get(CONTRACTS.erc20Mock);
+    const erc721MockDeployment = await deployments.get(CONTRACTS.erc721Mock);
+    const erc1155MockDeployment = await deployments.get(CONTRACTS.erc1155Mock);
 
     const authorityDeployment = await deployments.get(CONTRACTS.authority);
     const treasuryDeployment = await deployments.get(CONTRACTS.treasury);
     const escrowDeployment = await deployments.get(CONTRACTS.escrow);
-    
+
     const deployerAddress =  signer.getAddress();
     
     const authority = await Authority__factory.connect(authorityDeployment.address, signer);
@@ -33,6 +43,15 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     
     await authority.pushVault(treasury.address, true);
     console.log("Authority Vault:", await authority.vault());
+
+    const erc721Mock = await ERC721Mock__factory.connect(erc721MockDeployment.address, signer);
+    await erc721Mock.mint(deployerAddress);
+    console.log("Minted 1 ERC721 token to deployer");
+
+    const erc1155Mock = await ERC1155Mock__factory.connect(erc1155MockDeployment.address, signer);
+    await erc1155Mock.mint(deployerAddress, 1, 100, "0x0000000000000000000000000000000000000000");
+    console.log("Minted 1 ERC1155 token to deployer without metadata");
+
 
 }
     
