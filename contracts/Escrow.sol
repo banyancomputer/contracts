fa21ff5712b5b9b89b527d5748dc7c4636623d7a
Marco Jardim <marcoeojardim@gmail.com>
2022-07-27 19:51:39 -0300

    Escrow refactor to only support ERC20

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,17 +2,13 @@
 pragma solidity >= 0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
-import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "./types/AccessControlled.sol";
 import "./interfaces/ITreasury.sol";
 
 import "hardhat/console.sol";
 
-contract Escrow is Context, ERC1155Holder, ERC721Holder, AccessControlled
+contract Escrow is Context, AccessControlled
 {
     event NewOffer(address indexed creator, address indexed executor, uint256 offerId);
     event FinishOffer(address indexed executor, uint256 offerId);
@@ -32,30 +28,24 @@ contract Escrow is Context, ERC1155Holder, ERC721Holder, AccessControlled
 
     enum OfferStatus { NON, OFFER_CREATED, OFFER_COMPLETED, OFFER_CANCELLED  }
     enum UserStatus  { NON, OPEN, DEPOSIT, CLAIM }
-    enum OfferType   { NON, ERC20, ERC1155, ERC721, NATIVE }
 
     struct OfferCounterpart {
-        address contractAddr;
-        bytes32 proof;
-        uint256 idAsset;
+        bytes32 commitment;
         uint256 amount;
-        OfferType offerorType;
         UserStatus offerorStatus;
     }
     struct Offer {
+        address token;
         address creator;
+        OfferCounterpart creatorCounterpart;
         address executor;
+        OfferCounterpart executorCounterpart;
         uint256 id;
-        mapping(address => OfferUser) _offerors;
         string file;
         string description;
         OfferStatus offerStatus;
     }
-    struct OfferUser 
-    {
-         uint256[] tokenAddressIdx;
-         mapping(uint256 => OfferCounterpart ) _counterpart;
-    }
+
     constructor(address _authority) AccessControlled(IAuthority(_authority))
     {
         require(_authority != address(0));
@@ -66,71 +56,43 @@ contract Escrow is Context, ERC1155Holder, ERC721Holder, AccessControlled
         treasury = ITreasury(authority.vault());
     }
 
-     function startOffer(address[] memory creatorTokenAddress, uint256[] memory creatorTokenId, uint256[] memory creatorAmount, uint8[] memory creatorTokenType,
-                         address  executerAddress , address[] memory executorTokenAddress, uint256[] memory executorTokenId, uint256[] memory executorAmount, uint8[] memory executorTokenType) public payable returns(uint256)
+    // TODO: refactor using eip 4626
+     function startOffer(address token, uint256 creatorAmount, address  executerAddress, uint256 executorAmount) public payable returns(uint256)
     {
-        require(executerAddress != address(0), 'EXECUTER_ADDRESS_NOT_VALID' );
-        require(creatorTokenAddress.length == creatorTokenId.length && creatorTokenAddress.length ==  creatorAmount.length 
-                 && creatorTokenAddress.length == creatorTokenType.length , 'CREATOR_PARMS_LEN_ERROR');
-        require(executorTokenAddress.length == executorTokenId.length && executorTokenAddress.length ==  executorAmount.length 
-                 && executorTokenAddress.length == executorTokenType.length , 'EXECUTER_PARMS_LEN_ERROR');
+        require(executerAddress != address(0), 'EXECUTER_ADDRESS_NOT_VALID' );    
 
         _offerId++;
         _transactions[_offerId].id = _offerId;
+        _transactions[_offerId].token = token;
         _transactions[_offerId].creator = msg.sender;
         _transactions[_offerId].executor = executerAddress;
-        for (uint256 i = 0; i < creatorTokenAddress.length; i++) 
-        {
-            verifyOfferIntegrity(creatorTokenAddress[i], creatorTokenId[i], creatorAmount[i], creatorTokenType[i] );
-            if(OfferType(creatorTokenType[i]) == OfferType.ERC20){
-                verifyERC20(msg.sender, creatorTokenAddress[i], creatorAmount[i]);
-            }else if(OfferType(creatorTokenType[i]) == OfferType.ERC721){
-                verifyERC721(msg.sender, creatorTokenAddress[i], creatorTokenId[i]);
-            }else if(OfferType(creatorTokenType[i]) == OfferType.ERC1155){
-                verifyERC1155(msg.sender, creatorTokenAddress[i], creatorAmount[i], creatorTokenId[i]);
-            }
-            _transactions[_offerId]._offerors[msg.sender].tokenAddressIdx.push(i+1);
-            _transactions[_offerId]._offerors[msg.sender]._counterpart[i+1].contractAddr = creatorTokenAddress[i];
-            _transactions[_offerId]._offerors[msg.sender]._counterpart[i+1].idAsset = creatorTokenId[i];
-            _transactions[_offerId]._offerors[msg.sender]._counterpart[i+1].amount  = creatorAmount[i];
-            _transactions[_offerId]._offerors[msg.sender]._counterpart[i+1].offerorType = OfferType(creatorTokenType[i]);
-            _transactions[_offerId]._offerors[msg.sender]._counterpart[i+1].offerorStatus = UserStatus.OPEN;
-        }
-        for (uint i = 0; i < executorTokenAddress.length; i++) 
-        {
-            verifyOfferIntegrity(executorTokenAddress[i], executorTokenId[i], executorAmount[i], executorTokenType[i] );
-            if(OfferType(executorTokenType[i]) == OfferType.ERC20){
-                verifyERC20(executerAddress, executorTokenAddress[i], executorAmount[i]);
-            }else if(OfferType(executorTokenType[i]) == OfferType.ERC721){
-                verifyERC721(executerAddress, executorTokenAddress[i], executorTokenId[i]);
-            }else if(OfferType(executorTokenType[i]) == OfferType.ERC1155){
-                verifyERC1155(executerAddress, executorTokenAddress[i], executorAmount[i], executorTokenId[i]);
-            }
-            _transactions[_offerId]._offerors[executerAddress].tokenAddressIdx.push(i+1);
-            _transactions[_offerId]._offerors[executerAddress]._counterpart[i+1].contractAddr = executorTokenAddress[i];
-            _transactions[_offerId]._offerors[executerAddress]._counterpart[i+1].idAsset = executorTokenId[i];
-            _transactions[_offerId]._offerors[executerAddress]._counterpart[i+1].amount  = executorAmount[i];
-            _transactions[_offerId]._offerors[executerAddress]._counterpart[i+1].offerorType = OfferType(executorTokenType[i]);
-            _transactions[_offerId]._offerors[executerAddress]._counterpart[i+1].offerorStatus = UserStatus.OPEN;
-        }
+    
+        verifyOfferIntegrity(token, creatorAmount);
+        verifyERC20(msg.sender, token, creatorAmount);
+
+        // TODO: upgrade readability and maybe gas optimization
+        _transactions[_offerId].creatorCounterpart.amount  = creatorAmount;
+        _transactions[_offerId].creatorCounterpart.offerorStatus = UserStatus.OPEN;
+        
+        verifyOfferIntegrity(token, executorAmount);
+        verifyERC20(executerAddress, token, executorAmount);
+           
+        _transactions[_offerId].executorCounterpart.amount  = creatorAmount;
+        _transactions[_offerId].executorCounterpart.offerorStatus = UserStatus.OPEN;
+        
         _transactions[_offerId].offerStatus = OfferStatus.OFFER_CREATED;
         _openOffers[msg.sender].push(_offerId);
         _openOffers[executerAddress].push(_offerId);
 
         // Start moving funds to Treasury
-        for (uint256 i = 0; i < creatorTokenAddress.length; i++) {
-            // console.log("Escrow address", address(this));           
-            treasury.deposit{ value: msg.value }(creatorAmount[i], creatorTokenAddress[i], creatorTokenType[i], creatorTokenId, creatorAmount, msg.sender);
-        }
-
-        for (uint256 i = 0; i < executorTokenAddress.length; i++) {           
-            treasury.deposit{ value: msg.value }(executorAmount[i], executorTokenAddress[i], executorTokenType[i], executorTokenId, executorAmount, executerAddress);
-        }    
+        treasury.deposit(creatorAmount, token, msg.sender);
+        treasury.deposit(executorAmount, token, executerAddress);
 
         emit NewOffer(msg.sender, executerAddress, _offerId );
         return _offerId;
     }
 
+    // TODO: add chargeback and offer dispute logic
     function cancelOffer(uint256 offerId) public  returns (bool)
     {
         Offer storage store = _transactions[offerId];
@@ -146,50 +108,21 @@ contract Escrow is Context, ERC1155Holder, ERC721Holder, AccessControlled
         return (store.creator, store.executor, uint8(store.offerStatus), store.file);
     }
     
-    function getOffer(uint256 offerId, address userWallet) public view returns (address[] memory, bytes32[] memory, uint8[] memory )
-    {
-        Offer storage store = _transactions[offerId];
-        uint256[] memory tokenAddressIdx = store._offerors[userWallet].tokenAddressIdx;
+     function verifyERC20 (address from, address tokenAddress, uint256 amount) internal view returns (bool){
+        require(amount <= IERC20(tokenAddress).balanceOf(from), 'ERROR: ERR_NOT_ENOUGH_FUNDS_ERC20');
+        require(amount <= IERC20(tokenAddress).allowance(from, authority.vault() ), 'ERROR: ERR_NOT_ALLOW_SPEND_FUNDS');
+        return true;
+    }
 
-        address[]  memory _offerTokenAddress = new address[](tokenAddressIdx.length);
-        bytes32[]  memory _offerProof        = new bytes32[](tokenAddressIdx.length);
-        uint8[]    memory _offerType         = new uint8[](tokenAddressIdx.length);
-
-        for(uint y = 0; y < tokenAddressIdx.length; y++){
-                OfferCounterpart memory tInfo = store._offerors[userWallet]._counterpart[tokenAddressIdx[y]];
-                _offerTokenAddress[y]= msg.sender;
-                _offerProof[y] = tInfo.proof;
-                _offerType[y] = uint8(tInfo.offerorType);
-        }
-        return (_offerTokenAddress, _offerProof, _offerType  );
-    }
-    function verifyERC721 (address from, address tokenAddress, uint256 tokenId) internal view returns (bool){
-        require(from == ERC721(tokenAddress).ownerOf{gas:100000}(tokenId), 'ERROR: ERR_NOT_OWN_ID_ERC721');
-        require( ERC721(tokenAddress).isApprovedForAll{gas:100000}(from, authority.vault() ) , 'ERROR: ERR_NOT_ALLOW_TO_TRANSER_ITENS_ERC721');
-        return true;
-    }
-    function verifyERC20 (address from, address tokenAddress, uint256 amount) internal view returns (bool){
-        require(amount <= IERC20(tokenAddress).balanceOf{gas:100000}(from), 'ERROR: ERR_NOT_ENOUGH_FUNDS_ERC20');
-        require(amount <= IERC20(tokenAddress).allowance{gas:100000}(from, authority.vault() ), 'ERROR: ERR_NOT_ALLOW_SPEND_FUNDS');
-        return true;
-    }
-    function verifyERC1155 (address from, address tokenAddress, uint256 amount, uint256 tokenId) internal view returns (bool){
-        require(tokenId > 0, 'ERROR: STAKE_ERC1155_ID_SHOULD_GREATER_THEN_0');
-        require(amount > 0 && amount <= ERC1155(tokenAddress).balanceOf{gas:100000}(from, tokenId), 'ERROR: ERR_NOT_ENOUGH_FUNDS_ERC1155');
-        require( ERC1155(tokenAddress).isApprovedForAll{gas:100000}( from, authority.vault() ) , 'ERROR: ERR_NOT_ALLOW_TO_TRANSER_ITENS_ERC1155');
-        return true;
-    }
     
-    function verifyOfferIntegrity(address tokenAddress, uint256 tokenId,  uint256 amount, uint8 tokenType) public pure returns(bool)
+    function verifyOfferIntegrity(address tokenAddress,  uint256 amount) public pure returns(bool)
     {
         require(tokenAddress != address(0), 'ERROR: CREATOR_CONTRACT_NOT_VALID' );
-        require(tokenType >=0 && tokenType <= uint8(OfferType.NATIVE)  , 'ERROR: NOT_VALID_OFFER_TYPE');
-        require(tokenId >= 0  , 'ERROR: TOKENID_MUST_POSITIVE');
         require(amount > 0  , 'ERROR: AMOUNT_MUST_POSITIVE');
         return true;
     }
 
-    function offerPerUser(address u) public view returns(uint256[] memory ){
+    function offerPerUser(address u) public view returns(uint256[] memory ) {
         return (_openOffers[u]);
     }
 
--- test/escrow.ts
+++ test/escrow.ts
@@ -5,9 +5,7 @@ const { ethers } = require("hardhat");
 describe("Escrow", async () => {
 
   before(async function () {
-    this.ERC20Mock = await ethers.getContractFactory("ERC20Mock");
-    this.ERC721Mock = await ethers.getContractFactory("ERC721Mock");
-    this.ERC1155Mock = await ethers.getContractFactory("ERC1155Mock");
+    this.ERC20Mock = await ethers.getContractFactory("BanyanERC20Mock");
 
     this.Authority = await ethers.getContractFactory("Authority");
     this.Treasury = await ethers.getContractFactory("Treasury");
@@ -22,8 +20,6 @@ describe("Escrow", async () => {
 
   beforeEach(async function () {
     this.erc20Mock = await this.ERC20Mock.deploy();
-    this.erc721Mock = await this.ERC721Mock.deploy();
-    this.erc1155Mock = await this.ERC1155Mock.deploy();
     this.authority = await this.Authority.deploy(this.ownerAddress, this.ownerAddress, this.ownerAddress, this.ownerAddress);
     this.treasury = await this.Treasury.deploy(this.authority.address);
 
@@ -42,65 +38,12 @@ describe("Escrow", async () => {
     const approveExecutor = await this.erc20Mock.connect(this.executor).approve(this.treasury.address, 1000);
     await approveExecutor.wait();
 
-    // ERC721Mock stuff
-    const mintOwner = await this.erc721Mock.mint(this.ownerAddress);
-    await mintOwner.wait();
-    // console.log(await this.erc721Mock.ownerOf(1));
-    const mintExecutor = await this.erc721Mock.mint(this.executorAddress);
-    await mintExecutor.wait();
-    const approveOwner721 = await this.erc721Mock.setApprovalForAll(this.treasury.address, true);
-    await approveOwner721.wait();
-    const approveExecutor721 = await this.erc721Mock.connect(this.executor).setApprovalForAll(this.treasury.address, true);
-    await approveExecutor721.wait();
-
-    // ERC1155Mock stuff
-    const mintOwner1155 = await this.erc1155Mock.mint(this.ownerAddress, 1, 100, ethers.BigNumber.from(1));
-    await mintOwner1155.wait();
-    const mintExecutor1155 = await this.erc1155Mock.mint(this.executorAddress, 1, 100, ethers.BigNumber.from(1));
-    await mintExecutor1155.wait();
-    const approveOwner1155 = await this.erc1155Mock.setApprovalForAll(this.treasury.address, true);
-    await approveOwner1155.wait();
-    const approveExecutor1155 = await this.erc1155Mock.connect(this.executor).setApprovalForAll(this.treasury.address, true);
-    await approveExecutor1155.wait();
-
-
     this.offerParams = [
-      [this.erc20Mock.address], // creatorTokenAddress
-      [1], //creatorTokenId - used for ERC721 and ERC1155
-      [10], //creatorTokenAmount - used for ERC20 and ERC1155
-      [1], //creatorTokenType /// 1 == ERC20, 2 == ERC1155, 3 == ERC721
+      this.erc20Mock.address, // creatorTokenAddress
+      10, //creatorTokenAmount - used for ERC20 and ERC1155
       this.executorAddress, // executorAddress
-      [this.erc20Mock.address], // executorTokenAddress
-      [1], //executorTokenId - used for ERC721 and ERC1155
-      [10], //executorTokenAmount - used for ERC20 and ERC1155
-      [1], //executorTokenType /// 1 == ERC20, 2 == ERC1155, 3 == ERC721
+      10, //executorTokenAmount - used for ERC20 and ERC1155
     ];
-
-    this.offerParams721 = [
-      [this.erc721Mock.address], // creatorTokenAddress
-      [0], //creatorTokenId - used for ERC721 and ERC1155
-      [10], //creatorTokenAmount - used for ERC20 and ERC1155
-      [3], //creatorTokenType /// 1 == ERC20, 2 == ERC1155, 3 == ERC721
-      this.executorAddress, // executorAddress
-      [this.erc721Mock.address], // executorTokenAddress
-      [1], //executorTokenId - used for ERC721 and ERC1155
-      [10], //executorTokenAmount - used for ERC20 and ERC1155
-      [3], //executorTokenType /// 1 == ERC20, 2 == ERC1155, 3 == ERC721
-    ];
-
-    this.offerParams1155 = [
-      [this.erc1155Mock.address], // creatorTokenAddress
-      [1], //creatorTokenId - used for ERC721 and ERC1155
-      [10], //creatorTokenAmount - used for ERC20 and ERC1155
-      [2], //creatorTokenType /// 1 == ERC20, 2 == ERC1155, 3 == ERC721
-      this.executorAddress, // executorAddress
-      [this.erc1155Mock.address], // executorTokenAddress
-      [1], //executorTokenId - used for ERC721 and ERC1155
-      [10], //executorTokenAmount - used for ERC20 and ERC1155
-      [2], //executorTokenType /// 1 == ERC20, 2 == ERC1155, 3 == ERC721
-    ];
-
-
   });
     
   it("should be deployed", async function () {
@@ -115,30 +58,6 @@ describe("Escrow", async () => {
     const offerTx = await offer.wait();
 
     const offerResult = await this.EscrowInterface.decodeFunctionResult("startOffer", offerTx.logs[6].data);
-    const offerId = offerResult[0].toNumber();
-
-    expect(offerId).to.equal(1);
-    expect((await this.escrow.offerPerUser(this.ownerAddress))[0]).to.equal(offerId);
-  });
-
-  it("should start an ERC721 collateral offer", async function () {
-    
-    const offer = await this.escrow.startOffer(...this.offerParams721);
-    const offerTx = await offer.wait();
-
-    const offerResult = await this.EscrowInterface.decodeFunctionResult("startOffer", offerTx.logs[6].data);
-    const offerId = offerResult[0].toNumber();
-
-    expect(offerId).to.equal(1);
-    expect((await this.escrow.offerPerUser(this.ownerAddress))[0]).to.equal(offerId);
-  });
-
-  it("should start an ERC1155 collateral offer", async function () {
-    
-    const offer = await this.escrow.startOffer(...this.offerParams1155, { value: 0});
-    const offerTx = await offer.wait();
-
-    const offerResult = await this.EscrowInterface.decodeFunctionResult("startOffer", offerTx.logs[4].data);
     const offerId = offerResult[0].toNumber();
 
     expect(offerId).to.equal(1);
