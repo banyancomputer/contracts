cb0a068521d862ddbe9e9094a57257226330abf9
Marco Jardim <marcoeojardim@gmail.com>
2022-07-27 19:52:31 -0300

    ERC20 Mock contract name conflict bugfix

--- contracts/mocks/ERC20Mock.sol
+++ contracts/mocks/ERC20Mock.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
 
-contract ERC20Mock is ERC20PresetFixedSupply {
+contract BanyanERC20Mock is ERC20PresetFixedSupply {
     constructor () ERC20PresetFixedSupply("MockERC20", "MOCK", 1000000, msg.sender) {
     }
 }
