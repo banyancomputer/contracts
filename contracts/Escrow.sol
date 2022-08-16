3d61afa23a504c7a73b33ea117d63f1e3f0db6bb
Xiangan He <xiangan9@gmail.com>
2022-08-16 16:29:19 -0400

    refactor: forge integration

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -4,7 +4,7 @@ pragma solidity >= 0.8.9;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
-import '@chainlink/contracts/src/v0.8/ConfirmedOwner.sol';
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
 
 import "./types/AccessControlled.sol";
